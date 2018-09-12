2f3c4a428941c48c520c766492c9d656e2bfad06
Abhay Anand <>
2018-09-12 11:30:25 +0400

    Initial commit of the android project

---
+++ app/src/main/java/in/abhayanand/android/android_feature_toggle/MainActivity.java
@@ -0,0 +1,13 @@
+package in.abhayanand.android.android_feature_toggle;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
