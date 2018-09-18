a792e3e93d3a6a02d0727d22150da639cf84ac19
Abhay Anand <>
2018-09-18 10:20:12 +0400

    1. Called the validate user method from the branch app activity 2. Updated JDK version

--- app/src/branch/java/in/abhayanand/android/android_feature_toggle/BranchAppActivity.java
+++ app/src/branch/java/in/abhayanand/android/android_feature_toggle/BranchAppActivity.java
@@ -9,5 +9,7 @@ public class BranchAppActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_branch_app);
+
+        boolean isAuthenticated = Authentication.validateUser(this);
     }
 }
